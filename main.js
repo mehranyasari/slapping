6e6b6598697fe40ddbd422be89c44fd29c6f3757
mehranyasari <yasarimehran@gmail.com>
2024-01-27 15:41:32 +0330

    changes in structure

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 const countDownDate = new Date("05/25/2024 11:30 AM").getTime();
 // Update the count down every 1 second
 const x = setInterval(function () {
-    // today's date and time
-    const now = new Date().getTime();
+  // today's date and time
+  const now = new Date().getTime();
 
-    // distance between now and the count down date
-    const distance = countDownDate - now;
+  // distance between now and the count down date
+  const distance = countDownDate - now;
 
-    // Time calculations
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Time calculations
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result
-    document.getElementById(
-        "countdown"
-    ).innerHTML = `${days}day ${hours}hours ${minutes}minutes ${seconds}second `;
-    //  count down is finished, write something
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "NOWWWWWWWW!";
-    }
+  // Display the result
+  document.getElementById(
+    "countdown"
+  ).innerHTML = `${days}day ${hours}hours ${minutes}minutes ${seconds}second `;
+  //  count down is finished, write something
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "NOWWWWWWWW!";
+  }
 }, 1000);
